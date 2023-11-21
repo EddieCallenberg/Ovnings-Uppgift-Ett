f57caa6543fc0540adfe39ec388373832ece8935
Eddie <callenberg99@gmail.com>
2023-11-21 08:11:57 +0100

    -

--- Ovnings-Uppgift-Ett/Program.cs
+++ Ovnings-Uppgift-Ett/Program.cs
@@ -2,7 +2,17 @@
 while (isRunning)
 {
     Console.WriteLine("Hello, User!");
-    Console.WriteLine("Please Enter Your First Name:");
-    string userFirstName = Console.ReadLine();
+    Console.WriteLine("Please Enter Your Firstname:");
+    string userFirstName = Console.ReadLine() ?? string.Empty;
     Console.WriteLine("Hello " + userFirstName);
+    Console.WriteLine("Please Enter Your Lastname:");
+    string userLastName = Console.ReadLine() ?? string.Empty;
+    Console.WriteLine("Please Enter Your Age:");
+    int userAge;
+    while (!int.TryParse(Console.ReadLine(), out userAge))
+    {
+        Console.WriteLine("You Must Enter a Number As a Valid Age, Please Try again");
+    }
+    Console.WriteLine("Please Enter Your City or Town of Residence:");
+    string userCity = Console.ReadLine() ?? string.Empty;
 }
