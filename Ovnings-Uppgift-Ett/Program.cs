bcdcf0ba1fb4577858cf419971cf1936f883fa6e
Eddie <callenberg99@gmail.com>
2023-11-21 07:59:16 +0100

    Add project files.

---
+++ Ovnings-Uppgift-Ett/Program.cs
@@ -0,0 +1,8 @@
+﻿bool isRunning = true;
+while (isRunning)
+{
+    Console.WriteLine("Hello, User!");
+    Console.WriteLine("Please Enter Your First Name:");
+    string userFirstName = Console.ReadLine();
+    Console.WriteLine("Hello " + userFirstName);
+}
